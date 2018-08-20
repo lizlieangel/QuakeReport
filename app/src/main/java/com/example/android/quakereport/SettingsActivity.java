c56739450012bc43969c0551c83a51ca6149248f
Liz <lizlie.holleza@awsys-i.com>
2018-08-20 13:11:07 +0800

    add string rsc for order by

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -17,6 +19,9 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
         }
 
         @Override
@@ -24,6 +29,13 @@ public class SettingsActivity extends AppCompatActivity {
             String stringValue = o.toString();
             preference.setSummary(stringValue);
             return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = sharedPreferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
